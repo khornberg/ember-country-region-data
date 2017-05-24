63d3cc25f13d935460a13e486cb8d8fc1c3ab59b
Kyle Hornberg <kyle.hornberg@gmail.com>
2017-05-24 08:37:48 -0500

    Dependency: Update to latest ember 2.13

--- tests/unit/services/country-region-data-test.js
+++ tests/unit/services/country-region-data-test.js
@@ -4,7 +4,6 @@ moduleFor('service:country-region-data', 'Unit | Service | country region data',
 
 test('data', function(assert) {
   let service = this.subject();
-  console.log(service);
   assert.ok(service.data);
   assert.equal(service.data.length, 248);
   assert.equal(service.data[0].countryName, 'Afghanistan');
